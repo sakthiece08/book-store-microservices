3db75d7544119d089f774491cb44cecc256db6e7
sakthi <sakthiece08@gmail.com>
2024-04-23 16:45:06 -0400

    Initial project setup - Run test load context using RestAssured and Test container

---
+++ order-service/src/test/java/com/teqmonic/order/OrderServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.teqmonic.order;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.context.annotation.Bean;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.utility.DockerImageName;
+
+class OrderServiceApplicationTests extends AbstractIntegrationTest{
+
+	@Test
+	void contextLoads() {
+	}
+
+}
