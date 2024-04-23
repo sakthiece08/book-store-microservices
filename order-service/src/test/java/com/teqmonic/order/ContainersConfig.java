4e56b0f20c26e34bb307bff50d2207954fa3810b
sakthi <sakthiece08@gmail.com>
2024-04-23 17:03:52 -0400

    Add RabbitMQ container and Application test class

--- order-service/src/test/java/com/teqmonic/order/ContainersConfig.java
+++ order-service/src/test/java/com/teqmonic/order/ContainersConfig.java
@@ -4,6 +4,7 @@ import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Bean;
 import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.containers.RabbitMQContainer;
 import org.testcontainers.utility.DockerImageName;
 
 @TestConfiguration(proxyBeanMethods = false)
@@ -13,5 +14,11 @@ public class ContainersConfig {
     @ServiceConnection
     PostgreSQLContainer<?> postgreSQLContainer() {
         return new PostgreSQLContainer<>(DockerImageName.parse("postgres:16-alpine"));
+    }
+
+    @Bean
+    @ServiceConnection
+    RabbitMQContainer rabbitContainer() {
+        return new RabbitMQContainer(DockerImageName.parse("rabbitmq:3.13.1-alpine"));
     }
 }
