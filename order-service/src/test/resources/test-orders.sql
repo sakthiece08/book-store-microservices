5051098f40cf10d7a147dde3bd681f8406b7a902
sakthi <sakthiece08@gmail.com>
2024-07-23 16:07:01 -0400

    Fix test cases

--- order-service/src/test/resources/test-orders.sql
+++ order-service/src/test/resources/test-orders.sql
@@ -10,8 +10,8 @@ VALUES (1, 'TestUser1', 'TestUser1@test.com', '647-800-0900', '159 Aph st', '', 
 
 INSERT INTO public.orders
 (id, order_number, username, "comments", created_at, updated_at, status, customer_id)
-VALUES (1, 'ORD-100', 'user', null, '2024-05-06T12:14:33.142743', null, 'NEW', 1),
-       (2, 'ORD-101', 'user', null, '2024-05-07T14:14:33.142743', null, 'NEW', 1);
+VALUES (1, 'ORD-100', 'alex', null, '2024-05-06T12:14:33.142743', null, 'NEW', 1),
+       (2, 'ORD-101', 'alex', null, '2024-05-07T14:14:33.142743', null, 'NEW', 1);
 
 INSERT INTO public.order_items
 (id, code, "name", price, quantity, order_id)
